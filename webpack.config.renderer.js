d799c004214b68ce71cb9cb33dcbdf61333d13bb
ebith <ebith.h@gmail.com>
2018-04-12 22:19:54 +0900

    Add Content-Securiy-Policy

--- webpack.config.renderer.js
+++ webpack.config.renderer.js
@@ -2,6 +2,7 @@ const path = require('path');
 const common = require('./webpack.config.common.js');
 
 module.exports = {
+  devtool: 'cheap-module-source-map', // 【ChromeExtension】 'unsafe-eval' エラーの解決方法 - 人生dat落ち - http://eiua-memo.tumblr.com/post/172719308488/chromeextension-unsafe-eval-%E3%82%A8%E3%83%A9%E3%83%BC%E3%81%AE%E8%A7%A3%E6%B1%BA%E6%96%B9%E6%B3%95
   entry: './src/renderer.js',
   output: {
     filename: 'bundle.js',
